aa5fea1863b11675eeb1c34ce97c5b6f2b6c9703
MateusBonacinaZ <mateus.bonacina.b@gmail.com>
2022-04-27 13:55:27 -0300

    Simplificando a coleta de dados e removendo comentários

--- genetico.py
+++ genetico.py
@@ -40,41 +40,21 @@ class SmartTime:
                     numero_dias = int(str(configuracao).split(':')[1].strip())
 
         for termo in range(1, numero_termos+1):
+
+            disciplina = []
             for aula in df[f'{termo}° Termo']:
+
                 if pd.isnull(aula) is False:
                     infos = str(aula).split("/")
-                    if termo == 1:
-                        primeiro_termo.append((f'{infos[0].strip()}', f'{infos[1].strip()}', f'{int(str(infos[2]).strip())}'))
-                    elif termo == 2:
-                        segundo_termo.append((f'{infos[0].strip()}', f'{infos[1].strip()}', f'{int(str(infos[2]).strip())}'))
-                    elif termo == 3:
-                        terceiro_termo.append((f'{infos[0].strip()}', f'{infos[1].strip()}', f'{int(str(infos[2]).strip())}'))
-                    elif termo == 4:
-                        quarto_termo.append((f'{infos[0].strip()}', f'{infos[1].strip()}', f'{int(str(infos[2]).strip())}'))
-                    elif termo == 5:
-                        quinto_termo.append((f'{infos[0].strip()}', f'{infos[1].strip()}', f'{int(str(infos[2]).strip())}'))
-                    elif termo == 6:
-                        sexto_termo.append((f'{infos[0].strip()}', f'{infos[1].strip()}', f'{int(str(infos[2]).strip())}'))
+                    disciplina.append((f'{infos[0].strip()}', f'{infos[1].strip()}', f'{int(str(infos[2]).strip())}'))
 
-        primeiro_termo.sort(key=lambda x: x[2], reverse=True)
-        segundo_termo.sort(key=lambda x: x[2], reverse=True)
-        terceiro_termo.sort(key=lambda x: x[2], reverse=True)
-        quarto_termo.sort(key=lambda x: x[2], reverse=True)
-        quinto_termo.sort(key=lambda x: x[2], reverse=True)
-        sexto_termo.sort(key=lambda x: x[2], reverse=True)
-
-        informacoes_completas.append(primeiro_termo)
-        informacoes_completas.append(segundo_termo)
-        informacoes_completas.append(terceiro_termo)
-        informacoes_completas.append(quarto_termo)
-        informacoes_completas.append(quinto_termo)
-        informacoes_completas.append(sexto_termo)
+            disciplina.sort(key=lambda x: x[2], reverse=True)
+            informacoes_completas.append(disciplina)
 
         return tamanho_populacao, numero_termos, numero_aulas, numero_dias, informacoes_completas
 
     def gerar_populacao(self):
         populacao = []
-
 
         for individuo in range(self.tamanho_populacao):
             matriz_grade = []
@@ -89,8 +69,8 @@ class SmartTime:
                         matriz_grade[matriz_termos][matriz_dias].append(None)
 
             for termo in range(self.numero_termos):
-                disciplinas_termo = []
 
+                disciplinas_termo = []
                 for disciplina in self.informacoes_excel[termo]:
                     disciplinas_termo.append(disciplina)
 
@@ -126,139 +106,3 @@ class SmartTime:
 
 
 SmartTime()
-
-"""
-for aula in df["1° Termo"]:
-    if pd.isnull(aula) is False:
-        infos = str(aula).split("-")
-        primeiro_termo.append((f'{infos[0]}', f'{infos[1]}', f'{infos[2]}'))
-
-for aula in df["2° Termo"]:
-    if pd.isnull(aula) is False:
-        infos = str(aula).split("-")
-        segundo_termo.append((f'{infos[0]}', f'{infos[1]}', f'{infos[2]}'))
-
-for aula in df["3° Termo"]:
-    if pd.isnull(aula) is False:
-        infos = str(aula).split("-")
-        terceiro_termo.append((f'{infos[0]}', f'{infos[1]}', f'{infos[2]}'))
-
-for aula in df["4° Termo"]:
-    if pd.isnull(aula) is False:
-        infos = str(aula).split("-")
-        quarto_termo.append((f'{infos[0]}', f'{infos[1]}', f'{infos[2]}'))
-
-for aula in df["5° Termo"]:
-    if pd.isnull(aula) is False:
-        infos = str(aula).split("-")
-        quinto_termo.append((f'{infos[0]}', f'{infos[1]}', f'{infos[2]}'))
-
-for aula in df["6° Termo"]:
-    if pd.isnull(aula) is False:
-        infos = str(aula).split("-")
-        sexto_termo.append((f'{infos[0]}', f'{infos[1]}', f'{infos[2]}'))
-
-
-        for individuo in range(TAM_POPULACAO):
-            grade_list = []
-
-            for termo in range(NUM_TERMOS):
-
-                copia_termo = []
-                for disciplina in self.informacoes_excel[termo]:
-                    for aula in range(int(disciplina[2])):
-                        copia_termo.append((f'{disciplina[0]}', f'{disciplina[1]}'))
-
-                termo_list = []
-                for quantidade_aulas in range(len(copia_termo)):
-                    aleatorio = random.choice(copia_termo)
-                    copia_termo.remove(aleatorio)
-                    termo_list.append(aleatorio)
-
-                grade_list.append(termo_list)
-
-            populacao.append(grade_list)
-
-
-
-                    
-                    if int(disciplinas_termo[disciplina][2]) >= 4:
-                        if matriz_grade[termo][dia] == [None, None, None, None]:
-                            if int(disciplinas_termo[disciplina][2]) == 4:
-                                for x in range(self.numero_aulas):
-                                    matriz_grade[termo][dia][x] = (f'{disciplinas_termo[disciplina][0]}', f'{disciplinas_termo[disciplina][1]}')
-                                disciplinas_termo.remove(disciplinas_termo[disciplina])
-                            elif int(disciplinas_termo[disciplina][2]) > 4:
-                                for x in range(self.numero_aulas):
-                                    matriz_grade[termo][dia][x] = (f'{disciplinas_termo[disciplina][0]}', f'{disciplinas_termo[disciplina][1]}')
-                                disciplinas_termo[disciplina][2] = disciplinas_termo[disciplina][2] - 4
-                    
-
-
-
-
-                disciplinas_termo = []
-                for disciplina in self.informacoes_excel[termo]:
-                    for numero in range(int(disciplina[2])):
-                        disciplinas_termo.append((f'{disciplina[0]}', f'{disciplina[1]}'))
-
-                for dia in range(self.numero_dias):
-                    for aula in range(self.numero_aulas):
-                        if matriz_grade[termo][dia][aula] is None:
-                            contador = 0
-                            materia = random.choice(disciplinas_termo)
-                            while materia in disciplinas_termo:
-                                try:
-                                    if contador != 0:
-                                        matriz_grade[termo][dia][aula+contador] = materia
-                                    else:
-                                        matriz_grade[termo][dia][aula] = materia
-                                    disciplinas_termo.remove(materia)
-                                    contador += 1
-                                except IndexError:
-                                    break
-
-"""
-
-
-"""
-                    disciplina = random.randint(0, len(disciplinas_termo)-1)
-                    dia = random.randint(0, self.numero_dias - 1)
-
-                    print(len(disciplinas_termo))
-
-                    if int(disciplinas_termo[disciplina][2]) > 4:
-                        if matriz_grade[termo][dia].count(None) >= 4:
-                            for campo in range(self.numero_aulas):
-                                if matriz_grade[termo][dia][campo] is None:
-                                    matriz_grade[termo][dia][campo] = (f'{disciplinas_termo[disciplina][0]}', f'{disciplinas_termo[disciplina][1]}')
-                            disciplinas_termo[disciplina][2] = disciplinas_termo[disciplina][2] - self.numero_aulas
-
-                    elif int(disciplinas_termo[disciplina][2]) == 4:
-                        if matriz_grade[termo][dia].count(None) >= 4:
-                            for campo in range(self.numero_aulas):
-                                if matriz_grade[termo][dia][campo] is None:
-                                    matriz_grade[termo][dia][campo] = (f'{disciplinas_termo[disciplina][0]}', f'{disciplinas_termo[disciplina][1]}')
-                            disciplinas_termo.remove(disciplinas_termo[disciplina])
-
-                    elif int(disciplinas_termo[disciplina][2]) == 3:
-                        if matriz_grade[termo][dia].count(None) >= 3:
-                            for campo in range(self.numero_aulas):
-                                if matriz_grade[termo][dia][campo] is None:
-                                    matriz_grade[termo][dia][campo] = (f'{disciplinas_termo[disciplina][0]}', f'{disciplinas_termo[disciplina][1]}')
-                            disciplinas_termo.remove(disciplinas_termo[disciplina])
-
-                    elif int(disciplinas_termo[disciplina][2]) == 2:
-                        if matriz_grade[termo][dia].count(None) >= 2:
-                            for campo in range(self.numero_aulas):
-                                if matriz_grade[termo][dia][campo] is None:
-                                    matriz_grade[termo][dia][campo] = (f'{disciplinas_termo[disciplina][0]}', f'{disciplinas_termo[disciplina][1]}')
-                            disciplinas_termo.remove(disciplinas_termo[disciplina])
-
-                    elif int(disciplinas_termo[disciplina][2]) == 1:
-                        if matriz_grade[termo][dia].count(None) >= 1:
-                            for campo in range(self.numero_aulas):
-                                if matriz_grade[termo][dia][campo] is None:
-                                    matriz_grade[termo][dia][campo] = (f'{disciplinas_termo[disciplina][0]}', f'{disciplinas_termo[disciplina][1]}')
-                            disciplinas_termo.remove(disciplinas_termo[disciplina])
-"""
